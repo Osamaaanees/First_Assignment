a9381ada94629bc2862e3125c2f3e79cfb8e7998
Osamaaanees <uanees434@gmail.com>
2025-03-16 01:40:16 +0500

    Save changes before switching branches

---
+++ data_processing.py
@@ -0,0 +1,30 @@
+employees = [{"name": "Ehsan", "age": 35, "salary": 300000},
+    {"name": "Sajad", "age": 26, "salary": 195000},
+    {"name": "Rizwan", "age": 31, "salary": 185000},
+    {"name": "Imran", "age": 55, "salary": 37000},
+    {"name": "Usama", "age": 26, "salary": 85000},
+    {"name": "Imad", "age": 24, "salary": 55000},
+    {"name": "Hamza", "age": 24, "salary": 60000},
+]
+
+def group_employees_by_age(employees):
+    category = {"young": [], "mid": [], "senior": []}
+
+    for emp in employees:
+        if emp['age'] > 30:
+            category["young"].append(emp)
+        elif emp['age'] <= 30 and emp['age'] > 39:
+            category["mid"].append(emp)
+        else:
+            category["senior"].append(emp)
+    return category
+age_group = group_employees_by_age(employees)
+print ("Under 30 Employees")
+for emp in age_group["young"]:
+    print(f"{emp['name']} - {emp['age']}")    
+print ("Mid-Aged Employees")
+for emp in age_group["mid"]:
+    print(f"{emp['name']} - {emp['age']}")
+print ("Senior Employees")
+for emp in age_group["senior"]:
+    print(f"{emp['name']} - {emp['age']}")
