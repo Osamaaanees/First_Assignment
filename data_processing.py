7179fec77c160cc2dc9733fd3bd6ed7fe7e611e3
Osamaaanees <uanees434@gmail.com>
2025-03-16 20:00:52 +0500

    Updated data_processing.py with salary formatting

--- data_processing.py
+++ data_processing.py
@@ -1,23 +1,3 @@
-
-employees = [
-    {"name": "Ehsan", "age": 35, "salary": 300000},
-    {"name": "Sajad", "age": 26, "salary": 195000},
-    {"name": "Rizwan", "age": 31, "salary": 185000},
-    {"name": "Imran", "age": 55, "salary": 37000},
-    {"name": "Usama", "age": 26, "salary": 85000}
-    {"name": "Imad", "age": 24, "salary": 55000}
-    {"name": "Hamza", "age": 24, "salary": 60000}
-]
-
-def filter_high_salary(employees, threshold=60000):
-  return [emp for emp in employees if emp["salary"] > threshold]
-
-high_earners = filter_high_salary(employees)
-
-print("Employees earning more than 60,000:")
-for emp in high_earners:
-  print(f"{emp['name']} - Salary: {emp['salary']}")
-
 employees = [{"name": "Ehsan", "age": 35, "salary": 300000},
     {"name": "Sajad", "age": 26, "salary": 195000},
     {"name": "Rizwan", "age": 31, "salary": 185000},
@@ -48,3 +28,17 @@ for emp in age_group["mid"]:
 print ("Senior Employees")
 for emp in age_group["senior"]:
     print(f"{emp['name']} - {emp['age']}")
+
+
+def employees_salary(employees, threshold = 60000):
+    high_salary = {"high_earners" : []}
+
+    for emp in employees:
+        if emp["salary"] > threshold:
+            high_salary["high_earners"].append(emp)
+    return high_salary
+
+executive_employees = employees_salary(employees, threshold = 60000)
+print ("Higher Than 60000 Salary")
+for emp in executive_employees["high_earners"]:
+    print(f"{emp['name']} - {emp['salary']}")
